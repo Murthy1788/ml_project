0ac70f855d3294afd365ad38d2a6f58c528add1c
Murthy1788 <narayanamurthy.r990899@gmail.com>
2023-04-20 18:57:42 +0530

    Data Ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,7 @@
 import sys
-from src.logger import logging
+sys.path.insert(0, '../src')
+import unittest
+# from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error.detail.exc_info()
@@ -17,13 +19,7 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
 
-'''if __name__=="__main__":
 
-    try:
-        a=1/0
-    except Exception as e:
-        logging.info("logging has started")
-        raise CustomException(e,sys)'''
 
 
 
